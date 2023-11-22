5fbc6235d6eada52dcaeef6106623b5ee67cefa2
Rhys Bobbett <120944779+rhysbobbett@users.noreply.github.com>
2023-11-22 16:12:55 +0000

    Add SubCategory to products model

--- products/models.py
+++ products/models.py
@@ -16,9 +16,25 @@ class Category(models.Model):
         return self.friendly_name
 
 
+class SubCategory(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Sub Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True) # noqa
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
 class Product(models.Model):
-    category = models.ForeignKey(
-        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL) # noqa
+    sub_category = models.ForeignKey('SubCategory', on_delete=models.SET_NULL, null=True) # noqa
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
