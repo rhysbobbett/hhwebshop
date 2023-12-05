b242c48944633eec9d75c516f5db0ad2d69ef2e1
Rhys Bobbett <120944779+rhysbobbett@users.noreply.github.com>
2023-12-05 13:31:41 +0000

    Fix smtp connect

--- hhwebshop/settings.py
+++ hhwebshop/settings.py
@@ -101,7 +101,7 @@ if os.environ.get('DEBUG') == 'True':
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
-    EMAIL_PORT = os.environ.get('EMAIL_PORT')
+    EMAIL_PORT = 587
     EMAIL_HOST = os.environ.get('EMAIL_HOST')
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
