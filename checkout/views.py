1a4863d67fb3df536967f08628e9bc9984723510
Rhys Bobbett <120944779+rhysbobbett@users.noreply.github.com>
2023-12-05 15:22:18 +0000

    Change connection in views.py

--- checkout/views.py
+++ checkout/views.py
@@ -146,6 +146,8 @@ def checkout_success(request, order_number):
     """
     Handle successful checkouts
     """
+    connection = get_connection()
+    connection.open()
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
 
@@ -189,6 +191,7 @@ def checkout_success(request, order_number):
             [cust_email],
             connection=connection
         )
+    connection.close()
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
